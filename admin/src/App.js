8bd0ba6281ccb020b94e09966fcf2a1592d72bea
laibaafridi <laibaafridi.design@gmail.com>
2024-11-13 17:45:17 +0500

    My Admin Work

---
+++ admin/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import AddFood from './pages/Add/AddFood';
+import ListFood from './pages/List/ListFood';
+import Orders from './pages/Orders/Orders';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const url = "http://localhost:4000";  // backend URL
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      
+      <div className='app-cont'>
+        <Sidebar />
+        <Routes>
+          <Route path='/add' element={<AddFood url={url}/>} />
+          <Route path='/list' element={<ListFood url={url}/>} />
+          <Route path='/orders' element={<Orders url={url}/>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
